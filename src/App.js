b4e73172ec94706921b939e93569be691660c642
Danielle-Tal <danielle.tal1986@gmail.com>
2019-12-22 13:42:40 +0200

    Firebase deployed, fixed indentation.CSS need work

--- src/App.js
+++ src/App.js
@@ -1,81 +1,95 @@
-import React from 'react';
-import './App.css';
-import TweetBox from './components/TweetBox';
-import TweetList from './components/TweetList';
-import './components/Tweet';
-import MyAppContext from './components/MyAppContext';
-import { getTweetsList, createTweet} from './lib/API';
-import NavBar from './components/NavBar/index';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Profile from './components/Profile'
-
+import React from "react";
+import "./App.css";
+import TweetBox from "./components/TweetBox";
+import TweetList from "./components/TweetList";
+import "./components/Tweet";
+import MyAppContext from "./components/MyAppContext";
+import { getTweetsList, createTweet } from "./lib/API";
+import NavBar from "./components/NavBar/index";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Profile from "./components/Profile";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       tweets: [],
       addTweet: this.handleOnTweet.bind(this),
-      isLoading:true,
+      isLoading: true,
       errorMessage: false,
-      errorText: '',
-      }
-    }
-  
-    getTweets(app) {
-      getTweetsList().then(response => {
-      const tweets = response.data.tweets;
-      app.setState({ tweets: tweets, isLoading:false });
-      });
-    }
+      errorText: ""
+    };
+  }
 
-  componentDidMount(){
+  getTweets() {
+    getTweetsList().then(response => {
+      const tweet = response.data.tweets;
+      this.setState({ tweets: tweet, isLoading: false });
+    });
+  }
+
+  componentDidMount() {
     this.getTweets(this);
-    setInterval(() => {this.getTweets(this)}, 10000)};
+    setInterval(() => {
+      this.getTweets(this);
+    }, 10000);
+  }
 
   handleOnTweet(tweet) {
-    const tweetobj= {
+    const tweetobj = {
       content: tweet,
-      userName: localStorage.getItem('userName'),
-      date:new Date().toISOString(),}
+      userName: localStorage.getItem("userName"),
+      date: new Date().toISOString()
+    };
     createTweet(tweetobj)
-    .then(() => {
-      const {tweets} = this.state;
-    const tweetarray = [tweetobj, ...tweets]
-    this.setState({ tweets: tweetarray})
-    
-  },
-    )
-    .catch(response => {
-      console.log(response.response.data);
-      this.setState({ errorMessage: true ,errorText: response.response.data });
-    })
+      .then(() => {
+        const { tweets } = this.state;
+        const tweetarray = [tweetobj, ...tweets];
+        this.setState({ tweets: tweetarray });
+      })
+      .catch(response => {
+        console.log(response.response.data);
+        this.setState({
+          errorMessage: true,
+          errorText: response.response.data
+        });
+      });
   }
 
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-        <NavBar></NavBar>
-      <Switch>
-        <Route path="/home">
-          <MyAppContext.Provider value = {this.state}>
-            <TweetBox></TweetBox>
-            {this.state.errorMessage && <div className="errormessege" ><p className="error-messege-text">{this.state.errorText}</p></div>}
-            {this.state.isLoading && 
-             <img className="loader" src="http://pluspng.com/img-png/loader-png-22-jan-2015-23-46-3161-loader-6-png-22-jan-2015-23-46-3087-loader-7-png-22-jan-2015-23-46-3177-loader-8-png-22-jan-2015-23-46-3582-loader-9-png-504.png" alt="loader"></img>}
-            <TweetList>
-            </TweetList>
-            </MyAppContext.Provider>
-        </Route>
-        <Route path="/profile">
-          <Profile></Profile>
-        </Route>
-      </Switch>
-      </header>
-    </div>
-    </Router>
+        <div className="App">
+          <header className="App-header">
+            <NavBar></NavBar>
+            <Switch>
+              <Route path="/home">
+                <MyAppContext.Provider value={this.state}>
+                  <TweetBox></TweetBox>
+                  {this.state.errorMessage && (
+                    <div className="errormessege">
+                      <p className="error-messege-text">
+                        {this.state.errorText}
+                      </p>
+                    </div>
+                  )}
+                  {this.state.isLoading && (
+                    <img
+                      className="loader"
+                      src="http://pluspng.com/img-png/loader-png-22-jan-2015-23-46-3161-loader-6-png-22-jan-2015-23-46-3087-loader-7-png-22-jan-2015-23-46-3177-loader-8-png-22-jan-2015-23-46-3582-loader-9-png-504.png"
+                      alt="loader"
+                    ></img>
+                  )}
+                  <TweetList></TweetList>
+                </MyAppContext.Provider>
+              </Route>
+              <Route path="/profile">
+                <Profile></Profile>
+              </Route>
+            </Switch>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/TweetBox/index.js
+++ src/components/TweetBox/index.js
@@ -1,39 +1,56 @@
-import React from 'react'
-import './style.css'
-import MyAppContext from '../MyAppContext'
+import React from "react";
+import "./style.css";
+import MyAppContext from "../MyAppContext";
 
 class TweetBox extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            tweet: "",
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweet: ""
+    };
+  }
 
-    handleChange(event) {
-        this.setState({ tweet: event.target.value })
-    }
+  handleChange(event) {
+    this.setState({ tweet: event.target.value });
+  }
 
-    render() {
-        const { tweet } = this.state;
-        return (
-            <MyAppContext.Consumer>
-                {({ addTweet }) => (
-                    <div>
-                        <textarea className="createTweetText TweetBox" placeholder="What you have in mind..." value={tweet}
-                            onChange={(event) => this.handleChange(event)}>
-                        </textarea>
-                        {this.state.tweet.length > 140 && <div className="errormessege" ><p className="error-messege-text">The tweet can't contain more then 140 chars</p></div>}
-                        <button type="submit" className="createTweetButton buttonText" disabled={this.state.tweet.length > 140 || this.state.tweet.length === 0}
-                            onClick={() =>{ 
-                                addTweet(tweet);
-                                this.setState({tweet: ''});
-                            }
-                            } >Tweet</button>
-                    </div>)}
-            </MyAppContext.Consumer>
-        )
-    }
+  render() {
+    const { tweet } = this.state;
+    return (
+      <MyAppContext.Consumer>
+        {({ addTweet }) => (
+          <div>
+            <textarea
+              className="createTweetText TweetBox"
+              placeholder="What you have in mind..."
+              value={tweet}
+              onChange={event => this.handleChange(event)}
+            ></textarea>
+            {this.state.tweet.length > 140 && (
+              <div className="errormessege">
+                <p className="error-messege-text">
+                  The tweet can't contain more then 140 chars
+                </p>
+              </div>
+            )}
+            <button
+              type="submit"
+              className="createTweetButton buttonText"
+              disabled={
+                this.state.tweet.length > 140 || this.state.tweet.length === 0
+              }
+              onClick={() => {
+                addTweet(tweet);
+                this.setState({ tweet: "" });
+              }}
+            >
+              Tweet
+            </button>
+          </div>
+        )}
+      </MyAppContext.Consumer>
+    );
+  }
 }
 
 export default TweetBox;
